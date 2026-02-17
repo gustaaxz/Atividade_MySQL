38e8db4a0cb1db3c4a812026ad3d79c8b961f70f
gustaaxz <gustavo_hatschbach@estudante.sesisenai.org.br>
2026-02-17 09:49:40 -0300

    17/02/2026 - Realizado pequenas alterações dentro do código (inicializando ele)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,177 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.println("""
+                --- Sistema de Logística ---
+                1 - Cadastrar Cliente
+                2 - Cadastrar Motorista
+                3 - Criar Pedido
+                4 - Atribuir Pedido a Motorista (Gerar Entrega)
+                5 - Registrar Evento de Entrega (Histórico)
+                6 - Atualizar Status da Entrega
+                7 - Listar Todas as Entregas com Cliente e Motorista
+                8 - Relatórios Sistema 
+                9 - Buscar Pedido por Documento
+                10 - Cancelar Pedido
+                11 - Excluir Entrega (com validação)
+                12 - Excluir Cliente (com verificação de dependência)
+                13 - Excluir Motorista (com verificação de dependência)
+                0 - Sair
+                
+                Escolha uma opção: """);
+
+        int opcao = sc.nextInt();
+        sc.nextLine();
+
+        switch (opcao){
+            case 1: {
+                cadastrarCliente();
+            }
+
+            case 2: {
+                cadastrarMotorista();
+            }
+
+            case 3: {
+                criarPedido();
+            }
+
+            case 4: {
+                atribuirPedido();
+            }
+
+            case 5: {
+                registrarEventoEntrega();
+            }
+
+            case 6: {
+                atualizarStatusEntrega();
+            }
+
+            case 7: {
+                listarEntregas();
+            }
+
+            case 8: {
+                relatorioSistema();
+            }
+
+            case 9: {
+                buscarPedidoPorDocumento();
+            }
+
+            case 10: {
+                cancelarPedido();
+            }
+
+            case 11: {
+                excluirEntrega();
+            }
+
+            case 12: {
+                excluirCliente();
+            }
+
+            case 13: {
+                excluirMotorista();
+            }
+
+            case 0: {
+                System.exit(0);
+            }
         }
+    }
+
+
+    public static void cadastrarCliente() {
+    }
+
+    public static void cadastrarMotorista() {
+    }
+
+    public static void criarPedido() {
+    }
+
+    public static void atribuirPedido() {
+    }
+
+    public static void registrarEventoEntrega() {
+    }
+
+    public static void atualizarStatusEntrega() {
+    }
+
+    public static void listarEntregas() {
+    }
+
+    public static void relatorioSistema() {
+        System.out.println("""
+                --- Menu de Relatórios ---
+                1 - Total de Entregas por Motorista
+                2 - Clientes com Maior Volume Entregue
+                3 - Pedidos Pendentes por Estado
+                4 - Entregas Atrasadas por Cidade
+                0 - Voltar
+                
+                Escolha o relatório: 
+                """);
+
+        int opcaorelatorio = sc.nextInt();
+        sc.nextLine();
+
+        switch(opcaorelatorio) {
+            case 1 : {
+                relatorioEntregasPorMotorista();
+            }
+
+            case 2 : {
+                relatorioVolumePorCliente();
+            }
+
+            case 3 : {
+                relatorioPendentesPorEstado();
+            }
+
+            case 4 : {
+                relatorioAtrasosPorCidade();
+            }
+
+            case 0 : {
+                System.out.println("Saindo...");
+            } while (opcaorelatorio != 0);
+        }
+        sc.close();
+    }
+
+    private static void relatorioEntregasPorMotorista() {
+    }
+
+    private static void relatorioVolumePorCliente() {
+    }
+
+    private static void relatorioPendentesPorEstado() {
+    }
+
+    private static void relatorioAtrasosPorCidade() {
+    }
+
+    public static void buscarPedidoPorDocumento() {
+    }
+
+    public static void cancelarPedido() {
+    }
+
+    public static void excluirEntrega() {
+    }
+
+    public static void excluirCliente() {
+    }
+
+    public static void excluirMotorista() {
     }
 }
